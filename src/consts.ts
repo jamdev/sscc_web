e1f851b596824684d9813dada0077bb395215758
Chris James <flat.year0568@fastmail.com>
2025-09-07 16:10:46 +0100

    MVP version for discussion with members

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,149 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Stonnall & Shenstone Camera Club";
+export const SITE_DESCRIPTION =
+  "A photography club based in Staffordshire, UK.";
+
+// Define your documents here
+//
+//https://icon-sets.iconify.design/mdi/?icon-filter=document
+export const documents: Document[] = [
+  {
+    name: "Committee",
+    date: "2025-09-10",
+    url: "/documents/committee.docx",
+    icon: "/icons/people.svg",
+    category: "Club documents",
+    categoryIcon: "/icons/clubdocs.svg",
+  },
+  {
+    name: "Constitution",
+    date: "2025-09-10",
+    url: "/documents/constitution.docx",
+    icon: "/icons/constitution.svg",
+    category: "Club documents",
+  },
+  {
+    name: "Privacy notice",
+    date: "2025-09-10",
+    url: "/documents/privacynotice.docx",
+    icon: "/icons/privacy.svg",
+    category: "Club documents",
+  },
+];
+
+// Define your events here
+export const events = [
+  // September 2025
+  {
+    id: 1,
+    date: "2025-09-08",
+    title: "Stonnall & Shenstone Camera Club Meeting",
+    startTime: "19:30",
+    duration: 120,
+    location:
+      "Trinity Methodist Church, Shenstone, New Rd, Shenstone, Lichfield WS14 0NQ",
+    tags: ["regular-meeting"],
+    color: "#2563eb",
+    description: "Fortnightly camera club meeting",
+    url: "/regular-meetings",
+  },
+  {
+    id: 2,
+    date: "2025-09-22",
+    title: "Stonnall & Shenstone Camera Club Meeting",
+    startTime: "19:30",
+    duration: 120,
+    location:
+      "Trinity Methodist Church, Shenstone, New Rd, Shenstone, Lichfield WS14 0NQ",
+    tags: ["regular-meeting"],
+    color: "#2563eb",
+    description: "Fortnightly camera club meeting",
+    url: "/regular-meetings",
+  },
+
+  // October 2025
+  {
+    id: 3,
+    date: "2025-10-06",
+    title: "Stonnall & Shenstone Camera Club Meeting",
+    startTime: "19:30",
+    duration: 120,
+    location:
+      "Trinity Methodist Church, Shenstone, New Rd, Shenstone, Lichfield WS14 0NQ",
+    tags: ["regular-meeting"],
+    color: "#2563eb",
+    description: "Fortnightly camera club meeting",
+    url: "/regular-meetings",
+  },
+  {
+    id: 4,
+    date: "2025-10-20",
+    title: "Stonnall & Shenstone Camera Club Meeting",
+    startTime: "19:30",
+    duration: 120,
+    location:
+      "Trinity Methodist Church, Shenstone, New Rd, Shenstone, Lichfield WS14 0NQ",
+    tags: ["regular-meeting"],
+    color: "#2563eb",
+    description: "Fortnightly camera club meeting",
+    url: "/regular-meetings",
+  },
+
+  // November 2025
+  {
+    id: 5,
+    date: "2025-11-03",
+    title: "Stonnall & Shenstone Camera Club Meeting",
+    startTime: "19:30",
+    duration: 120,
+    location:
+      "Trinity Methodist Church, Shenstone, New Rd, Shenstone, Lichfield WS14 0NQ",
+    tags: ["regular-meeting"],
+    color: "#2563eb",
+    description: "Fortnightly camera club meeting",
+    url: "/regular-meetings",
+  },
+  {
+    id: 6,
+    date: "2025-11-17",
+    title: "Stonnall & Shenstone Camera Club Meeting",
+    startTime: "19:30",
+    duration: 120,
+    location:
+      "Trinity Methodist Church, Shenstone, New Rd, Shenstone, Lichfield WS14 0NQ",
+    tags: ["regular-meeting"],
+    color: "#2563eb",
+    description: "Fortnightly camera club meeting",
+    url: "/regular-meetings",
+  },
+
+  // December 2025
+  {
+    id: 7,
+    date: "2025-12-01",
+    title: "Stonnall & Shenstone Camera Club Meeting",
+    startTime: "19:30",
+    duration: 120,
+    location:
+      "Trinity Methodist Church, Shenstone, New Rd, Shenstone, Lichfield WS14 0NQ",
+    tags: ["regular-meeting"],
+    color: "#2563eb",
+    description: "Fortnightly camera club meeting",
+    url: "/regular-meetings",
+  },
+  {
+    id: 8,
+    date: "2025-12-15",
+    title: "Stonnall & Shenstone Camera Club Meeting",
+    startTime: "19:30",
+    duration: 120,
+    location:
+      "Trinity Methodist Church, Shenstone, New Rd, Shenstone, Lichfield WS14 0NQ",
+    tags: ["regular-meeting"],
+    color: "#2563eb",
+    description: "Fortnightly camera club meeting",
+    url: "/regular-meetings",
+  },
+];
