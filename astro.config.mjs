0897999d9308d69bd5512e638b014064eff07452
Chris James <flat.year0568@fastmail.com>
2025-09-25 18:45:53 +0100

    Migrated to Cloudflare Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,20 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.sscc.org.uk/",
   integrations: [mdx(), sitemap()],
+  adapter: cloudflare({
+    imageService: "noop",
+    platformProxy: {
+      enabled: true,
+      configPath: "wrangler.jsonc",
+      persist: {
+        path: "./.cache/wrangler/v3",
+      },
+    },
+  }),
 });
